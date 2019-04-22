c60d32cd2b473d0b0b5b21ddac94f773a786ab09
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-22 14:37:58 -0400

    building basic structure...

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function changeToLightmode() {
+  document.body.style.backgroundColor = "white";
+}
+function changeToDarkMode() {
+  document.body.style.backgroundColor = "black";
+  document.getElementById("heading_title").style.color = "#ff0000";
+}

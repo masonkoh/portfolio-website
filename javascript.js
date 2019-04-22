a385be21c800bd67ac3b39028160d724b028d91c
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-22 19:31:22 -0400

    Darkmode button re-located

--- javascript.js
+++ javascript.js
@@ -1,14 +1,34 @@
 function changeToLightmode() {
+  var links = document.getElementsByTagName("a");
   document.body.style.backgroundColor = "white";
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href) {
+      links[i].style.color = "black";
+    }
+  }
+  document.getElementsByTagName("ul")[0].style.color = "black";
+  document.getElementById("grid_main_content").style.color = "black";
 }
+
+// var as = document.getElementsByTagName("a");
+// for (var i = 0; i < as.length; i++) {
+//   as[i].style.color = "white";
+// }
+// document.getElementsByTagName("ol")[0].style.color = "white";
+
 function changeToDarkMode() {
   var links = document.getElementsByTagName("a");
 
-  document.body.style.backgroundColor = "black";
+  document.body.style.backgroundColor = "#3A3C3E";
 
+  // for (var i = 0; i < links.length; i++) {
+  //   if (links[i].href) {
+  //     links[i].style.color = "#DCDCDC";
+  //   }
+  // }
   for (var i = 0; i < links.length; i++) {
-    if (links[i].href) {
-      links[i].style.color = "#DCDCDC";
-    }
+    links[i].style.color = "#DCDCDC";
   }
+  document.getElementsByTagName("ul")[0].style.color = "#DCDCDC";
+  document.getElementById("grid_main_content").style.color = "#DCDCDC";
 }

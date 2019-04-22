ce9d32ebded8466c022031c9683855485cb57b21
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-22 16:46:29 -0400

    Dark Mode toggle is left there unfinished

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@ function changeToLightmode() {
   document.body.style.backgroundColor = "white";
 }
 function changeToDarkMode() {
+  var links = document.getElementsByTagName("a");
+
   document.body.style.backgroundColor = "black";
-  document.getElementById("heading_title").style.color = "#ff0000";
+
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].href) {
+      links[i].style.color = "#DCDCDC";
+    }
+  }
 }

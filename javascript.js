c83855bd0f9eb01932c66182a9bf86a43fa21242
ko.terra.119@gmail.com <ko.terra.119@gmail.com>
2019-04-22 20:28:10 -0400

    comment in javascript.js is re-arranged

--- javascript.js
+++ javascript.js
@@ -9,25 +9,20 @@ function changeToLightmode() {
   document.getElementsByTagName("ul")[0].style.color = "black";
   document.getElementById("grid_main_content").style.color = "black";
 }
-// var as = document.getElementsByTagName("a");
-// for (var i = 0; i < as.length; i++) {
-//   as[i].style.color = "white";
-// }
-// document.getElementsByTagName("ol")[0].style.color = "white";
-
 function changeToDarkMode() {
   var links = document.getElementsByTagName("a");
 
   document.body.style.backgroundColor = "#3A3C3E";
 
-  // for (var i = 0; i < links.length; i++) {
-  //   if (links[i].href) {
-  //     links[i].style.color = "#DCDCDC";
-  //   }
-  // }
   for (var i = 0; i < links.length; i++) {
     links[i].style.color = "#DCDCDC";
   }
   document.getElementsByTagName("ul")[0].style.color = "#DCDCDC";
   document.getElementById("grid_main_content").style.color = "#DCDCDC";
 }
+
+// var as = document.getElementsByTagName("a");
+// for (var i = 0; i < as.length; i++) {
+//   as[i].style.color = "white";
+// }
+// document.getElementsByTagName("ol")[0].style.color = "white";
